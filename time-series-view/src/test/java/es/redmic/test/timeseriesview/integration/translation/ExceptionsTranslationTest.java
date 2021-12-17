1b6425df735a6c4cbf39ceff3f034af894719ac0
Noel <noelalonso1986@gmail.com>
2021-12-17 12:01:02 +0000

    Adapta test por cambio en traducciones

--- time-series-view/src/test/java/es/redmic/test/timeseriesview/integration/translation/ExceptionsTranslationTest.java
+++ time-series-view/src/test/java/es/redmic/test/timeseriesview/integration/translation/ExceptionsTranslationTest.java
@@ -9,9 +9,9 @@ package es.redmic.test.timeseriesview.integration.translation;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -82,7 +82,7 @@ public class ExceptionsTranslationTest {
 	}
 
 	@Test
-	public void getRussianLanguageMessage_returnDefaultI18nMessage_WhenI18nFileNotExist() {
+	public void getRussianLanguageMessage_returnCode_WhenI18nFileNotExist() {
 
 		try {
 			throw new ItemNotFoundException("id", "2");
@@ -94,7 +94,7 @@ public class ExceptionsTranslationTest {
 
 			String mess = messageSource.getMessage(code, fields, new Locale("ru", "RU"));
 
-			Assert.assertEquals(getMessage(code, fields, resourcePathSpanish), mess);
+			Assert.assertEquals(code, mess);
 		}
 	}
 
